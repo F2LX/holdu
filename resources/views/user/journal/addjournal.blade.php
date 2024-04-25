b8631eeded6b5fc1f8964734ebed022aa362dccb
F2LX <felixfebryan16@gmail.com>
2024-04-25 22:05:13 +0700

    Fix missing form header

--- resources/views/user/journal/addjournal.blade.php
+++ resources/views/user/journal/addjournal.blade.php
@@ -4,7 +4,8 @@
     <div class="container mt-5">
         <h3 class="fw-bold">Create a new Journal</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatem nobis quo repudiandae voluptates excepturi modi, sunt perferendis? Quaerat fuga reiciendis magnam deleniti fugit ipsam quos ipsum. Aliquam officiis et vero sint numquam praesentium? Officia tempore ducimus odit pariatur perferendis?</p>
-
+        <form action="/journal/add/post" method="post">
+          @csrf
         <h3 class="fw-bold">Journal Title</h3>
         <div class="input-group mb-3">
             <input type="text" name="title" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
@@ -13,6 +14,7 @@
         <div class="input-group">
             <textarea class="text-area rounded-4" name="content" aria-label="With textarea"></textarea>
           </div>
-        <button class="btn btn-success mt-4">Post</button>
+        <button type="submit" class="btn btn-success mt-4">Post</button>
+        </form>
     </div>
 @endsection
