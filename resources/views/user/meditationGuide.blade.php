f09d7a150bb2ac4e7390fa1a6878c9b05f16c9f1
Eneper <3nver.2510@gmail.com>
2024-04-25 22:43:04 +0700

    Profile,Reminder,Meditation

---
+++ resources/views/user/meditationGuide.blade.php
@@ -0,0 +1,32 @@
+@extends('ui.home')
+
+@section('content')
+    <div class="container">
+
+        <p class="mb-0 mt-5">Monday , 3 December 2024</p>
+        <h3 class="fw-bold mb-0">Meditation Guide</h3>
+        <p>Your Journey to Inner Peace with HoldU</p>
+
+        <div class="container">
+            <div class="row gap-5 w-100">
+                <div class="col-4 rounded-5 d-flex justify-content-center align-items-center p-5 bg-primary">
+                    <h2>Nature Sound</h2>
+                </div>
+                <div class="col-7 rounded-5 d-flex justify-content-center align-items-center p-5 bg-primary">
+                    <h2>Nature Sound</h2>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex">
+            <ol class="mt-5 ps-3">
+                <li><span class="fw-bold">Settle In:</span> Find a comfortable position, close your eyes, and take deep breaths, letting tension melt away.</li>
+                <li><span class="fw-bold">Body Scan:</span> Scan your body for tension, releasing it with each exhale, allowing deep relaxation.</li>
+                <li><span class="fw-bold">Focus on Breath:</span> Shift your attention to the sensation of breathing, gently returning focus if thoughts arise.</li>
+                <li><span class="fw-bold">Cultivate Stillness:</span> Breathe deeply, sinking into calm, letting go of worries, and being fully present.</li>
+                <li><span class="fw-bold">Embrace Peace:</span> Envision peace washing over you, bringing tranquility and contentment.</li>
+                <li><span class="fw-bold">Reflect and Give Thanks:</span> Take a moment to reflect on the experience, expressing gratitude for this moment of serenity.</li>    
+            </ol>
+        </div>
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,18 @@ Route::get('/profile',function(){
     return view('user.profile');
 });
 
+Route::get('/reminder',function(){
+    return view('user.reminder');
+});
+
+Route::get('/reminder',function(){
+    return view('user.reminder');
+});
+
+Route::get('/mediGuide',function(){
+    return view('user.meditationGuide');
+});
+Route::get('/mediTrack',function(){
+    return view('user.meditationTrack');
+});
 // ConnectU User Handler
