c8ef770c2c106537ee1e40966d1be3a5391959c5
F2LX <felixfebryan16@gmail.com>
2024-04-25 23:16:20 +0700

    Install OpenAI-PHP

---
+++ database/migrations/2024_04_25_155803_create_chats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->boolean('is_bot');
+            $table->foreignId('sender_id');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
